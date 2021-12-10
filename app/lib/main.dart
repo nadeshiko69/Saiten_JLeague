daa3271957033c9cb82b68de5b22651b956a2b83
Nadeshiko69 <ke.sakurauchi@gmail.com>
2021-12-11 00:22:14 +0900

    MainPage作業中 J1チーム一覧、ボタンの活性/非活性の選択

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -30,14 +30,215 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  bool _isEnabled = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
+          width: double.infinity,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    ButtonTheme(
+                      minWidth: 170.0,
+                      child: RaisedButton(
+                        child: Text('コンサドーレ札幌'),
+                        onPressed:  !_isEnabled ? null : () {
+                          // 何かEnableの時の処理
+                        },
+                      ),
+                    ),
 
+                    ButtonTheme(
+                      minWidth: 170.0,
+                      child: RaisedButton(
+                        child: Text('浦和レッズ'),
+                        onPressed:  !_isEnabled ? null : () {
+                          // 何かEnableの時の処理
+                        },
+                      ),
+                    ),
+
+                    ButtonTheme(
+                      minWidth: 170.0,
+                      child: RaisedButton(
+                        child: Text('FC東京'),
+                        onPressed:  !_isEnabled ? null : () {
+                          // 何かEnableの時の処理
+                        },
+                      ),
+                    ),
+
+                    ButtonTheme(
+                      minWidth: 170.0,
+                      child: RaisedButton(
+                        child: Text('横浜F・マリノス'),
+                        onPressed:  !_isEnabled ? null : () {
+                          // 何かEnableの時の処理
+                        },
+                      ),
+                    ),
+
+                    ButtonTheme(
+                      minWidth: 170.0,
+                      child: RaisedButton(
+                        child: Text('清水エスパルス'),
+                        onPressed:  !_isEnabled ? null : () {
+                          // 何かEnableの時の処理
+                        },
+                      ),
+                    ),
+
+                    ButtonTheme(
+                      minWidth: 170.0,
+                      child: RaisedButton(
+                        child: Text('名古屋グランパス'),
+                        onPressed: (){// !_isEnabled ? null : () {
+                          // 何かEnableの時の処理
+                        },
+                      ),
+                    ),
+
+                    ButtonTheme(
+                      minWidth: 170.0,
+                      child: RaisedButton(
+                        child: Text('セレッソ大阪'),
+                        onPressed:  !_isEnabled ? null : () {
+                          // 何かEnableの時の処理
+                        },
+                      ),
+                    ),
+
+                    ButtonTheme(
+                      minWidth: 170.0,
+                      child: RaisedButton(
+                        child: Text('ヴィッセル神戸'),
+                        onPressed:  !_isEnabled ? null : () {
+                          // 何かEnableの時の処理
+                        },
+                      ),
+                    ),
+
+                    ButtonTheme(
+                      minWidth: 170.0,
+                      child: RaisedButton(
+                        child: Text('アビスパ福岡'),
+                        onPressed:  !_isEnabled ? null : () {
+                          // 何かEnableの時の処理
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    ButtonTheme(
+                      minWidth: 170.0,
+                      child: RaisedButton(
+                        child: Text('鹿島アントラーズ'),
+                        onPressed:  !_isEnabled ? null : () {
+                          // 何かEnableの時の処理
+                        },
+                      ),
+                    ),
+
+                    ButtonTheme(
+                      minWidth: 170.0,
+                      child: RaisedButton(
+                        child: Text('柏レイソル'),
+                        onPressed:  !_isEnabled ? null : () {
+                          // 何かEnableの時の処理
+                        },
+                      ),
+                    ),
+
+                    ButtonTheme(
+                      minWidth: 170.0,
+                      child: RaisedButton(
+                        child: Text('川崎フロンターレ'),
+                        onPressed:  !_isEnabled ? null : () {
+                          // 何かEnableの時の処理
+                        },
+                      ),
+                    ),
+
+                    ButtonTheme(
+                      minWidth: 170.0,
+                      child: RaisedButton(
+                        child: Text('湘南ベルマーレ'),
+                        onPressed:  !_isEnabled ? null : () {
+                          // 何かEnableの時の処理
+                        },
+                      ),
+                    ),
+
+                    ButtonTheme(
+                      minWidth: 170.0,
+                      child: RaisedButton(
+                        child: Text('ジュビロ磐田'),
+                        onPressed:  !_isEnabled ? null : () {
+                          // 何かEnableの時の処理
+                        },
+                      ),
+                    ),
+
+                    ButtonTheme(
+                      minWidth: 170.0,
+                      child: RaisedButton(
+                        child: Text('ガンバ大阪'),
+                        onPressed:  !_isEnabled ? null : () {
+                          // 何かEnableの時の処理
+                        },
+                      ),
+                    ),
+
+                    ButtonTheme(
+                      minWidth: 170.0,
+                      child: RaisedButton(
+                        child: Text('京都サンガFC'),
+                        onPressed:  !_isEnabled ? null : () {
+                          // 何かEnableの時の処理
+                        },
+                      ),
+                    ),
+
+                    ButtonTheme(
+                      minWidth: 170.0,
+                      child: RaisedButton(
+                        child: Text('サンフレッチェ広島'),
+                        onPressed:  !_isEnabled ? null : () {
+                          // 何かEnableの時の処理
+                        },
+                      ),
+                    ),
+
+                    ButtonTheme(
+                      minWidth: 170.0,
+                      child: RaisedButton(
+                        child: Text('サガン鳥栖'),
+                        onPressed:  !_isEnabled ? null : () {
+                          // 何かEnableの時の処理
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
       drawer: Drawer(
         child: ListView(
