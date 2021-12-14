21e04b9b8978b62f1804b53909795b29ac00125e
Nadeshiko69 <ke.sakurauchi@gmail.com>
2021-12-14 22:46:24 +0900

    beautifulsoup導入 / スクレイピングでスタメン取得厳しい？

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,1 +1,9 @@
+from bs4 import BeautifulSoup
+import requests
 
+# site_data = requests.get("https://soccer.yahoo.co.jp/jleague/category/j1/game/2021022801/summary")
+ 
+# print(site_data.text)
+
+# scrapingでスタメン取得するの難しそう？
+# とりあえず手動前提で作る
