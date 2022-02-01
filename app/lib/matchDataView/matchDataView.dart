ae2c4effc85d737c692553fe5edb0f3da3e1f260
nadeshiko69 <shuckle.mawile@gmail.com>
2022-02-01 23:52:37 +0900

    スタメン、サブ情報取得機能追加

--- app/lib/matchDataView/matchDataView.dart
+++ app/lib/matchDataView/matchDataView.dart
@@ -14,6 +14,52 @@ class cMatchDetailView extends StatelessWidget {
   // TODO : あとでFactory.dartにきれいに実装する
   @override
   void GetMatchMember() async {
+    int STARTING   = 1; // スタメン
+    int SUBSTITUTE = 0; // サブ
+    int NONMEMBER  = -1;// ベンチ外
+    String matchNoIdx = "match" + matchNo.toString();
+
+    // 初期化
+    lStartingMemberData.clear();
+    lSubMemberData.clear();
+
+    // スタメンを取得
+    final _startingData = FirebaseFirestore.instance
+        .collection('Data')
+        .doc(teamName)
+        .collection('Member')
+        .where(matchNoIdx, isEqualTo: STARTING);
+
+    final QuerySnapshot<Map<String, dynamic>> startingSnapshot = await _startingData.get();
+
+    lStartingMemberData = startingSnapshot.docs.map((DocumentSnapshot document){
+      Map<String, dynamic> data = document.data() as Map<String, dynamic>;
+      final String name = data['name'];
+      final int number = data['number'];
+      return cPlayerData(name, number);
+    }).toList();
+
+    // サブを取得
+    final _subData = FirebaseFirestore.instance
+        .collection('Data')
+        .doc(teamName)
+        .collection('Member')
+        .where(matchNoIdx, isEqualTo: SUBSTITUTE);
+
+    final QuerySnapshot<Map<String, dynamic>> subSnapshot = await _subData.get();
+
+    lSubMemberData = subSnapshot.docs.map((DocumentSnapshot document){
+      Map<String, dynamic> data = document.data() as Map<String, dynamic>;
+      final String name = data['name'];
+      final int number = data['number'];
+      return cPlayerData(name, number);
+    }).toList();
+
+    print(lStartingMemberData.length);
+    print(lSubMemberData.length);
+  }
+
+  void GetMatchInfo() async {
     final _matchData = FirebaseFirestore.instance
         .collection('Data')
         .doc(teamName)
@@ -22,7 +68,7 @@ class cMatchDetailView extends StatelessWidget {
 
     final QuerySnapshot<Map<String, dynamic>> snapshot = await _matchData.get();
 
-    final matchData = snapshot.docs.map((DocumentSnapshot document){
+    final lMatchData = snapshot.docs.map((DocumentSnapshot document){
       Map<String, dynamic> data = document.data() as Map<String, dynamic>;
       final String opponent = data['opponent'];
       final String day = data['date'].toString();
@@ -30,10 +76,11 @@ class cMatchDetailView extends StatelessWidget {
       return cMatchData(opponent, day, matchNo);
     }).toList();
 
-    print(matchData[0].matchNo);
-    print(matchData[0].day);
-    print(matchData[0].opponent);
+    print(lMatchData[0].matchNo);
+    print(lMatchData[0].day);
+    print(lMatchData[0].opponent);
   }
+
   @override
   Widget build(BuildContext context) {
     GetMatchMember();
--- app/lib/matchDataView/matchDataViewData.dart
+++ app/lib/matchDataView/matchDataViewData.dart
@@ -1,4 +1,21 @@
 
 import 'package:judge/mainData.dart';
 
+// 試合情報を格納
 List<cMatchData> lMatchData = [];
+
+// 選手情報を格納
+class cPlayerData {
+  cPlayerData(this.name, this.number);
+  String name;
+  int    number;
+}
+
+// スタメン
+List<cPlayerData> lStartingMemberData = [];
+
+// サブ
+List<cPlayerData> lSubMemberData = [];
+
+
+
