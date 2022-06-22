40bc99fabe2fd0c2ae80377bca294d989d96054f
Nadeshiko69 <ke.sakurauchi@gmail.com>
2022-06-22 09:07:40 +0900

    作業中、firebaseから選手情報を読み込む関数追加

--- adminWeb/function/operateFirebase.py
+++ adminWeb/function/operateFirebase.py
@@ -1,34 +1,51 @@
 from firebase_admin import firestore
 import firebase_admin
 from firebase_admin import credentials
+from pyparsing import LRUMemo
 
 class operateFirebase():
     def __init__(self):
-        self.word = "Hello world."
         JSON_PATH = 'saitenj-408a1-firebase-adminsdk-r2bv3-1e3090988e.json'
         # Firebase初期化
         self.cred = credentials.Certificate(JSON_PATH)
         firebase_admin.initialize_app(self.cred)
         self.db = firestore.client()
-    
-    def print_word(self):
-        print(self.word)
-        
-    def writeMemberDataToFirebase(self):
+        self.lMemberList = []
+      
+    # 試合開始前にスタメン / ベンチ情報を登録する   
+    def fWriteMemberDataToFirebase(self):
         # コレクションにアクセス
         doc_ref = self.db.collection('news')
         try:
             doc_ref.add({
-                'title': 'a',
-                'url'  : 'b'
+                'starting': 'a',
+                'score'  : '-1'
                 })
             
             print("Done")
         except:
             print("Error")
+        # TODO：登録メンバーが18人になっていなければWarningを出す
+    
+    # スタメン/ベンチ情報登録のために、メンバーリストを読み込む　
+    def fReadMemberDataFromFirebase(self, teamName):
+        self.lMemberList.clear()
+        
+        doc_ref = self.db.collection('Data2022').document(teamName).collection('Member')
+        docs = doc_ref.stream()
+        for doc in docs:
+            # print(f"id:{doc.id}")
+            # print(f"Name:{doc.get('name')}")
+            personalData = {
+                id:{doc.id},
+                name:{doc.get('name')},
+                number:{doc.get('number')},
+                position:{doc.get('position')}
+            }
+
         
 # FOR DEBUG
 
 of = operateFirebase()
-of.print_word()
-of.writeMemberDataToFirebase()
+of.fWriteMemberDataToFirebase()
+of.fReadMemberDataFromFirebase('Nagoya')
