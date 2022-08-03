35266ad6e5072f751549960eff65040b62f8e89f
Nadeshiko69 <ke.sakurauchi@gmail.com>
2022-08-04 08:03:46 +0900

    メンバー情報の描画処理完成

--- app/lib/matchDataView/matchDataViewFactory.dart
+++ app/lib/matchDataView/matchDataViewFactory.dart
@@ -1,6 +1,8 @@
 /// matchDataViewFactory.dart
 /// matchDataView.dartで使用する関数定義
 
+import 'dart:convert';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:judge/mainData.dart';
@@ -19,30 +21,23 @@ Future<List<CPlayerData>> fGetMatchMember(
   List<CPlayerData> lMemberData = [];
 
   // 試合情報のDBから登録メンバーのIDを取得
-  final baseCollection = FirebaseFirestore.instance
+  await FirebaseFirestore.instance
       .collection('Data2022')
       .doc(teamName)
       .collection('Match')
       .doc(matchID)
-      .collection('Member').get();
+      .collection('Member').get()
+  // 取得したIDから選手情報を取得
+      .then((QuerySnapshot querySnapshot) async {
+        for (var doc in querySnapshot.docs) {
+          if(doc["starting"].toString() == isStarting.toString()){
+            await fGetMemberInfoForMemberID(teamName, doc.id).then((result) {
+              lMemberData.add(result);
+            } );
+          }
+        }
+      });
 
-  await baseCollection.then(
-        (QuerySnapshot querySnapshot) => {
-          querySnapshot.docs.forEach(
-            (doc) async {
-              if(doc.get("starting") == isStarting.toString()) {
-                //  fGetMemberInfoForMemberID(teamName, doc.id).then((result) {
-                //   lMemberData.add(result);
-                //   print(result);
-                //   print(lMemberData.length);
-                // });
-                lMemberData.add(await fGetMemberInfoForMemberID(teamName, doc.id));
-                print("w");
-              }
-            },
-          ),
-        },
-      );
   print("unko");
   return lMemberData;
 }
