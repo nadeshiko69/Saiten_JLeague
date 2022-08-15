4e54f60d3c3f137ade0404515ad011fbd43c8bb4
Nadeshiko69 <ke.sakurauchi@gmail.com>
2022-08-16 00:10:36 +0900

    送信済採点情報の更新機能を追加 / UserIDをFirebaseAuthから取得できるよう修正

--- app/lib/matchDataView/matchDataViewFactory.dart
+++ app/lib/matchDataView/matchDataViewFactory.dart
@@ -4,6 +4,7 @@
 //import 'dart:convert';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 //import 'package:judge/mainData.dart';
 import 'package:judge/matchDataView/matchDataViewData.dart';
 
@@ -15,8 +16,6 @@ Func : 該当する試合の登録メンバーを取得
 * */
 Future<List<CPlayerData>> fGetMatchMember(
     String teamName, String matchID, int matchNo, bool isStarting) async {
-  // String matchNoIdx = "match" + matchNo.toString();
-  // int memberCond = -1;
   List<CPlayerData> lMemberData = [];
 
   // 試合情報のDBから登録メンバーのIDを取得
@@ -25,17 +24,18 @@ Future<List<CPlayerData>> fGetMatchMember(
       .doc(teamName)
       .collection('Match')
       .doc(matchID)
-      .collection('Member').get()
-  // 取得したIDから選手情報を取得
+      .collection('Member')
+      .get()
+      // 取得したIDから選手情報を取得
       .then((QuerySnapshot querySnapshot) async {
-        for (var doc in querySnapshot.docs) {
-          if(doc["starting"].toString() == isStarting.toString()){
-            await fGetMemberInfoForMemberID(teamName, doc.id).then((result) {
-              lMemberData.add(result);
-            } );
-          }
-        }
-      });
+    for (var doc in querySnapshot.docs) {
+      if (doc["starting"].toString() == isStarting.toString()) {
+        await fGetMemberInfoForMemberID(teamName, doc.id).then((result) {
+          lMemberData.add(result);
+        });
+      }
+    }
+  });
   return lMemberData;
 }
 
@@ -44,15 +44,17 @@ Name : fGetMemberInfoForMemberID()
 Arg  : void
 Func : 選手のIDから選手名等の情報を取得
 * */
-Future<CPlayerData> fGetMemberInfoForMemberID(String teamName, String memberID) async {
+Future<CPlayerData> fGetMemberInfoForMemberID(
+    String teamName, String memberID) async {
   final DocumentSnapshot docSnapshot = await FirebaseFirestore.instance
       .collection('Data2022')
       .doc(teamName)
       .collection('Member')
-      .doc(memberID).get();
+      .doc(memberID)
+      .get();
   String name = docSnapshot.get("name");
   // String pos  = docSnapshot.get("position");
-  int    num  = docSnapshot.get("number");
+  int num = docSnapshot.get("number");
   CPlayerData returnData = CPlayerData(memberID, name, num);
 
   return returnData;
@@ -64,7 +66,7 @@ Arg  : void
 Func : 採点結果をfirebaseに格納
 * */
 void fSubmit(String teamName, String matchID) async {
-  String userID = "tID"; // FOR DEBUG
+  String? userID = FirebaseAuth.instance.currentUser?.uid; // FOR DEBUG
 
   // 既に採点情報が格納されているか確認して、あれば新規追加ではなく更新
   final QuerySnapshot snapshot = await FirebaseFirestore.instance
@@ -72,13 +74,13 @@ void fSubmit(String teamName, String matchID) async {
       .doc('Scores')
       .collection(teamName)
       .where('userID', isEqualTo: userID)
-      .where('matchID', isEqualTo: matchID)
+      .where('MatchID', isEqualTo: matchID)
       .get();
 
   // まだ一度も送信していない(snapshotのサイズが0)場合、新規追加
   if (snapshot.size == 0) {
+    int index = 0;
     for (CPlayerData member in lStartingList!) {
-      int index = 0;
       // スタメンの採点情報
       await FirebaseFirestore.instance
           .collection('Data2022')
@@ -94,8 +96,8 @@ void fSubmit(String teamName, String matchID) async {
       index++;
     }
     // サブの採点情報
+    index = 11;
     for (CPlayerData member in lSubList!) {
-      int index = 11;
       await FirebaseFirestore.instance
           .collection('Data2022')
           .doc('Scores')
@@ -107,12 +109,42 @@ void fSubmit(String teamName, String matchID) async {
         'userID': userID, // TODO:後で直す。UserIDをfirebase authからDB格納できるようになってから
         'score': lSelectedPointList![index]
       }); // データ
-
       index++;
     }
   }
 
   // 既に送信済なら情報をさいしんに更新する
-  else{} // TODO : 今日やる
-}
+  // TODO: 実装汚すぎるので後でいい感じにまとめる
+  else {
+    print("unko");
+    int index = 0;
+    for (CPlayerData member in lStartingList!) {
+      await FirebaseFirestore.instance
+          .collection('Data2022')
+          .doc('Scores')
+          .collection(teamName)
+          .where('userID', isEqualTo: userID)
+          .where('MatchID', isEqualTo: matchID)
+          .where('MemberID', isEqualTo: member.mid)
+          .get()
+          .then((QuerySnapshot snapshot) => {
+                snapshot.docs.forEach((f) {
+                  // TODO：where.getで現状forEachでループ（1回）を回すしかない。要検討
+                  FirebaseFirestore.instance
+                      .collection('Data2022')
+                      .doc('Scores')
+                      .collection(teamName)
+                      .doc(f.reference.id)
+                      .set({
+                    'MemberID': member.mid,
+                    'MatchID': matchID,
+                    'userID': userID, // TODO:後で直す。UserIDをfirebase authからDB格納できるようになってから
+                    'score': lSelectedPointList![index]
+                  });
+                })
+              });
 
+      index++;
+    }
+  }
+}
